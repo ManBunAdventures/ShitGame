f7fa1ddc7812cdcd50208a0a59b9bc9cf38d870f
Big-Jono <jonodnelson@gmail.com>
2014-07-05 19:48:11 -0700

    added stupid game damn it
    git pls

---
+++ Assets/ShootingScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootingScript : MonoBehaviour 
+{
+
+	public Rigidbody projectile;
+	public float speed = 5f;
+	
+	void ShootFireball () 
+	{
+		Rigidbody clone = (Rigidbody) Instantiate(projectile, transform.position, transform.rotation);
+		clone.velocity = transform.forward * speed;
+		
+		Destroy(clone.gameObject,3.0f);
+	}
+	
+	// Calls the fire method when holding down ctrl or mouse
+	void Update () 
+	{
+		if (Input.GetKeyUp(KeyCode.Q))
+			ShootFireball();
+	}
+}
